11fa270d560ce76041b05fbb6b2b862ea19ed1cc
rschreib <rschreib@mymail.mines.edu>
2019-08-29 13:50:31 -0600

    optimizations

--- ip_email_sender/email_sender.py
+++ ip_email_sender/email_sender.py
@@ -3,6 +3,7 @@ from subprocess import Popen, PIPE
 import smtplib
 from socket import gaierror
 import string
+import time
 
 # This script attepts to retrieve the IP address of a specified interface.
 # If successful it then emails the retrieved IP to a specified email address
@@ -25,36 +26,51 @@ RECEIVER['addr'] = [ 'bodogcheese@gmail.com' ];
 INTERFACE = 'wlan0' # default interface for a Raspberry Pi
 
 # specify the name of the computer to display in the email
-HOSTNAME = Popen(['hostname'], stdout=PIPE).communicate()[0].replace('\n', '')
+HOSTNAME_raw = Popen('hostname', stdout=PIPE)
+# HOSTNAME = str(HOSTNAME_raw.communicate()[0].replace('\n', ''))
+HOSTNAME = HOSTNAME_raw.communicate()[0].decode('utf-8').replace('\n','')
+
 
 #######################################################################
 
 # Execute the call to ifconfig 
 # Use AWK to remove everything but the IP Address
-try:
-	p1 = Popen(['ifconfig', INTERFACE], stdout=PIPE)
-	p2 = Popen(['awk', "/inet/ {split ($2,A,\":\"); print A[2]}"], stdout=PIPE, stdin=p1.stdout)
-	
-	the_ip = p2.communicate()[0].replace('\n','');
-except:
-	the_ip = 'FAILED'
+# do this continously
+while(True):
+	try:
+		# a more complicated way fo doing it
+		# p1 = Popen(['ifconfig', INTERFACE], stdout=PIPE)
+		# p2 = Popen(['awk', "/inet/ {split ($2,A,\":\"); print A[2]}"], stdout=PIPE, stdin=p1.stdout)
+		# output = p1.communicate()[0].decode('utf-8').split(' ')
+		# index = output.index('inet')
+		# the_ip = output[index+1]
+		p1 = Popen(['hostname', '-I'], stdout=PIPE)
+		the_ip = p1.communicate()[0].decode('utf-8').replace('\n','')
+		print(the_ip)
+		break
+	except:
+		the_ip = 'FAILED'
+		print("Failed to get inet")
+		print("attempting to find again in 1 second")
+		time.sleep(1)
 
-if the_ip =='':
-	the_ip = 'no address found'
+	if the_ip =='':
+		the_ip = 'no address found'
+		print("Failed to get inet")
+		print("attempting to find again in 1 second")
+		time.sleep(1)
 
-SUBJECT = "%s's IP:%s on:%s" % (HOSTNAME, the_ip, INTERFACE)
-SUBJECT = "{}'s IP:{} on:{}".format(HOSTNAME, the_ip, INTERFACE)
+
+
+
+SUBJECT = "{}'s IP on {}".format(HOSTNAME, INTERFACE)
 
 # Construct the Email
 TO = RECEIVER['addr']
 FROM = SENDER['addr'] 
-BODY = string.join((
-	'From: %s' % FROM,
-	'To: %s' % TO,
-	'Subject: %s' % SUBJECT,
-	'',
-	''
-	), '\r\n')
+BODY = the_ip
+
+BODY = "Subject: {}\n\n{}".format(SUBJECT, BODY)
 
 # Try to send the email
 try:
@@ -62,9 +78,10 @@ try:
 	server.login( SENDER['addr'], SENDER['pass'] )
 	server.sendmail( FROM, TO, BODY )
 	server.quit()
+	print('sent email. exiting.')
 
 except smtplib.SMTPAuthenticationError:
-	print ("Error, authentication failed! Please check your username and password.")
+	print("Error, authentication failed! Please check your username and password.")
 
 except gaierror:
-	print 'Error, cannot connect to: %s!  Please ensure it is a valid smtp server.' %(SENDER['serv'])
+	print('Error, cannot connect to: {}!  Please ensure it is a valid smtp server.'.format(SENDER['serv']))
