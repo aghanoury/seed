3f2606ccb8a464cf593502b594c510ac03cebaef
Pooya Aghanoury <pooya.aghanoury@gmail.com>
2019-10-19 22:31:01 -0600

    finished some stuff

--- demo1/main.py
+++ demo1/main.py
@@ -5,16 +5,25 @@ from src import Comms
 """ Main Runner Script for Demo 1 """
 
 
+# robot parameters
+r = 0.15/2
+d = 0.275
+
 com = Comms("CREAMSOUP\nSUPERBOT AI")
 # com.startup_color_sequence()
 
 # Send some data
-payload = [com.CHANGE_ANGLE, np.pi/2]
+# com.sendData(payload)
+
+# exit(0)
+
+
 
 commands = {1: com.CHANGE_ANGLE, 2: com.CHANGE_POS}
 
 while True:
-    print("Commands\n1: Change angle\n2: Change position")
+    payload = []
+    print("----\nCommands\n1: Change angle\n2: Change position")
     try:
         command = commands[int(input("Enter a command: "))] 
     except KeyboardInterrupt:
@@ -25,6 +34,26 @@ while True:
         continue
 
     if command == com.CHANGE_ANGLE:
-        angle = float(input("Targeted Angle: "))
-    print("Trash")
+        payload = [com.CHANGE_ANGLE]
 
+        angle1 = float(input("Targeted Angle Left (Deg): "))
+        # angle2 = float(input("Targeted Angle Right (Deg): "))
+        angle1 *= np.pi/180 
+        # angle2 *= np.pi/180 * 2
+
+        theta = angle1 * d / r
+        payload.append(theta)
+        payload.append(theta)
+
+        com.sendData(payload)
+
+    elif command == com.CHANGE_POS:
+        payload = [com.CHANGE_POS]
+
+        dist = float(input("Targeted Distance (ft): "))
+        dist = dist/3.281
+        theta = dist / r * 2
+        payload.append(-theta)
+        payload.append(theta)
+
+        com.sendData(payload)
