93775b459adb3865dbea4958f49359110a27ed49
Pooya Aghanoury <poo@csm-wl-dhcp-239-168.mines.edu>
2019-11-13 12:40:27 -0700

    optimizations, functionality

--- demo2/main.py
+++ demo2/main.py
@@ -4,49 +4,42 @@ from src import Finder
 import time
 import RPi.GPIO as gpio
 
+
+
+""" Main Runner Script for Demo 2 """
+
+f = Finder() # init CV object
+# f.start() # starts its own thread
+
+
+
+# init comms obj
+try:
+    # you can init comms with robot parameters
+    # we are using defualt values
+    com = Comms() 
+except:
+    print("FAILED TO INIT COMMS\nIs arduino on?")
+
+
+
+# configure GPIO to read robots status
+# eventually should move into comms class but had a weird
+# issue with calling GPIO functions from a parent scope
 gpio.setmode(gpio.BCM)
 gpio.setup(17, gpio.IN)
 
-
-""" Main Runner Script for Demo 1 """
-
-# change to true to run cv
-if True:
-    f = Finder() # init CV object
-    # f.start() # starts its own thread
-
-
-# robot parameters
-r = 78
-d = 69
-
-# init comms obj
-com = Comms()
-time.sleep(1)
-# com.start_state_detection()
-# try:
-#     com = Comms("CREAMSOUP\nSUPERBOT AI")
-# except:
-#     print("Failed to init Comms class")
-#     print("Is the arduino on and i2c connected?\nExiting")
-#     exit(14)
-# com.startup_color_sequence()
-
+# custom routines, values are arbitrary
 PRINT_CV = 69
 FIND_N_GO = 70
 REQUEST = 80
-
-# small dictionary of commands
-# add more commands to the comms class
+# get prebuilt commands
 cmds = com.commands
-
 # added tmp commands
 cmds['debug CV'] = PRINT_CV
 cmds['FIND_N_GO'] = FIND_N_GO
 cmds['request data'] = REQUEST
 
-# commands = {"angle change": com.CHANGE_ANGLE, "linear traverse": com.}
-# commands = {1: com.ROTATE, 2: com.LINEAR_TRAVERSE, 3: PRINT_CV, 4: FIND_N_GO}
 
 
 # continously prompt for user commands
@@ -60,6 +53,9 @@ while True:
     # get user input
     try:
         inp = input("Commands: ")
+        if inp == 'e':
+            print("EXITING")
+            exit(0)
         key = list(cmds)[int(inp)-1]
         command = cmds[key]
     except KeyboardInterrupt:
@@ -94,7 +90,7 @@ while True:
             target_radius = float(input("Target Radius (ft) "))
             target_radius /= 3.281
 
-            direction = input("l or r? (default is l, ccl): ")
+            direction = input("l or r? (press enter for defautl): ")
             
         except: print("Invalid Value")
         
@@ -135,60 +131,63 @@ while True:
 
     elif command == FIND_N_GO:
 
-        while True:
-            if(input("Auto Detect [y/n]: ") == 'y'):
-                timeout = time.time() + 25   # 5 minutes from now
-                f.find_markers()
-                com.search()
-                try:
-                    while f.did_detect == False:
-                        com.rotate(25)
-                        time.sleep(0.5)
-                        f.find_markers()
-                        if time.time() > timeout:
-                            print('Timeout')
-                            break
-                        
-                except KeyboardInterrupt:
-                    print("Keyboard interrupt. Returning to Home")
-                    com.stop()
-                    break
-                
-                com.stop()
+        # get some flags
+        direction = input('Search left or right [l/r]: ')
+        rotate = input('Rotate at end [y/n]: ')
 
+
+        while True:
+            try:
+                timeout = time.time() + 25
+                f.find_markers()
+                # com.search() 
+                # begin serach
+                while f.did_detect == False:
+                    if direction == 'r':
+                        com.rotate(-25)
+                    else:
+                        com.rotate(25)
+                
+                    wait(0.1)
+                    f.find_markers()
+                    if time.time() > timeout:
+                        print('Timeout')
+                        break
+                        
+                com.stop()
                 time.sleep(2)
                 if not f.did_detect:
                     print("Did not find a marker")
                     break
                 
-                result = f.markers
-                distance = round(result[0][0]/100,3)
-                angle = round(-result[0][1],3)
+                distance = round(f.markers[0][0]/100,3)
+                angle = round(-f.markers[0][1],3)
                 
-                try:
-                    print(distance, angle)
-                    com.rotate(angle, radians=True)
-                    time.sleep(0.2)
-                    while gpio.input(17) == False: pass
+                print("Found Marker {} meters {} radians".format(distance, angle))
+                com.rotate(angle, radians=True)
+                wait(0.2)
 
-                    com.linTraverse((distance)-0.40,meters=True)
-                    time.sleep(0.2)
-                    while gpio.input(17) == False: pass
+                com.linTraverse((distance)-0.35,meters=True)
+                wait(0.2)
 
-                    distance = round(result[0][0]/100,3)
-                    print("Distance: ", distance)
-                    com.rotate(-90)
-                    time.sleep(0.2)
-                    while gpio.input(17) == False: pass
+                distance = round(result[0][0]/100,3)
+                print("Distance: ", distance)
+                com.rotate(-90)
+                wait(0.2)
 
-                    com.circularTraverse(1.4*0.3048, direction='left')
-                    time.sleep(0.2)
-                    while gpio.input(17) == False: pass
+                com.circularTraverse(1.4*0.3048, direction='left')
+                wait(0.2)
 
-                except KeyboardInterrupt:
-                    print("Keyboard interrupt. Returning to Home")
-                    com.stop()
-                    break
-
-            else:
+            except KeyboardInterrupt:
+                print("Keyboard interrupt. Returning to Home")
+                com.stop()
                 break
+
+
+def wait(delay=0.3, timeout=15):
+    timout = time.time() + timeout
+    time.sleep(delay)
+    while gpio.input(17) == False:
+        if time.time() > timeout:
+            print("FAILED TO VERIFY MOTION TERMINATION\nEXITING")
+            exit(-1) 
